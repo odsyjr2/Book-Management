21b81211ef6b630c8d071c5c895f60590a8d71ab
tk032 <tedyang032@gmail.com>
2025-05-30 15:34:05 +0900

    OPEN AI 연동 및 도서 등록 페이지2

--- frontend/bookmanage/src/App.js
+++ frontend/bookmanage/src/App.js
@@ -1,16 +1,24 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import BookDetailPage from "./BookDetailPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   // page, setPage 선언
   const [page, setPage] = useState("add"); // 'add' 또는 'list'
-
+  
+  useEffect(() => {
+    if (window.location.pathname === "/books") {
+      setPage("add");
+    } else {
+      setPage("home");
+    }
+  }, []);
 
   // page 가 add일 때
   return (
     <div className="App">
-      {page === "add" && <BookDetailPage onBookCreated={() => setPage("list")} />}
+      {page === "add" && <BookDetailPage />}
     </div>
   );
 }
