8557518901238a1c732fbce445136bf9fe1c0750
KS-YEONI <kangsy5959@naver.com>
2025-05-30 15:09:31 +0900

    add component

---
+++ frontend/bookmanage/src/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+const Navbar = () => {
+    const menuList = ['Home','BookList','작픔 등록']
+  return (
+    <div>
+      <div className='nav-logo'>
+        <img width={150} src='./logo.png'/>
+      </div>
+      <div className='search'>
+            <div className='search-bar'>
+                 <FontAwesomeIcon icon={faSearch}/>
+                <input type="text" placeholder='검색할 작품명을 넣어주세요요'/>
+            </div>
+           
+        </div>
+      <div className='menu-area'>
+        <ul className='menu-list'>
+            {menuList.map((menu)=>(<li>{menu}</li>))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
