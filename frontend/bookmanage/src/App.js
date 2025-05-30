6b0cc422d04532fb61a364a0f36db51528f95cad
tk032 <tedyang032@gmail.com>
2025-05-30 15:10:01 +0900

    1

--- frontend/bookmanage/src/App.js
+++ frontend/bookmanage/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import BookDetailPage from "./BookDetailPage";
 
 function App() {
+
+  // page, setPage 선언
+  const [page, setPage] = useState("add"); // 'add' 또는 'list'
+
+
+  // page 가 add일 때
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {page === "add" && <BookDetailPage onBookCreated={() => setPage("list")} />}
     </div>
   );
 }
