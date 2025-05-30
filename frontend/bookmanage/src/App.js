77d3ce2856ede35a4a81bdef7367242ed04bc67d
KS-YEONI <kangsy5959@naver.com>
2025-05-30 16:33:29 +0900

    ver.1

--- frontend/bookmanage/src/App.js
+++ frontend/bookmanage/src/App.js
@@ -1,17 +1,23 @@
-import {Link} from 'react-router-dom'
-import './App.css';
-import Home from './Home'
-import BookList from './Booklist';
-import BookDetail from './BookDetail';
-import Navbar from './Navbar';
-import routers from './routers';
-import { RouterProvider } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import MainPage from './pages/MainPage';
+import NewBook from './pages/NewBook';
+import GenerateImage from './pages/GenerateImage';
+import BookListPage from './pages/BookListPage';
+import BookDetailPage from './pages/BookDetailPage'; // 선택
 
 function App() {
   return (
-    <div className="App">
-      <RouterProvider router={routers}/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/new-book" element={<NewBook />} />
+        <Route path="/generate-image" element={<GenerateImage />} />
+        <Route path="/books" element={<BookListPage />} />
+
+      </Routes>
+    </Router>
   );
 }
 
